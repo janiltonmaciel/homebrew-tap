84dccc0f4ad8ad06c05fb803e5cdd8ef69177ef0
goreleaserbot <goreleaser@carlosbecker.com>
2018-07-02 01:13:45 -0300

    Brew formula update for dockerfile-gen version 1.10.0

--- Formula/dockerfile-gen.rb
+++ Formula/dockerfile-gen.rb
@@ -1,9 +1,9 @@
 class DockerfileGen < Formula
   desc "Generator Dockerfile"
   homepage "https://github.com/janiltonmaciel/dockerfile-gen"
-  url "https://github.com///releases/download/1.9.0/dockerfile-gen_1.9.0_macOS_amd64.tar.gz"
-  version "1.9.0"
-  sha256 "5e1127bac499f36cc765c3967f6acd2d05f12bb248a46d15aec4fca837344490"
+  url "https://github.com/janiltonmaciel/dockerfile-gen/releases/download/1.10.0/dockerfile-gen_1.10.0_macOS_amd64.tar.gz"
+  version "1.10.0"
+  sha256 "d09e19aee212909ca3cc3ec769618b2ed5c020e0891e92d8a3bcda2c90648eb9"
 
   def install
     bin.install "dockerfile-gen"

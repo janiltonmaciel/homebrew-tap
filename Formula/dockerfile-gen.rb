bea46caca2f2f1587e1a30b2b70d79bc28234da2
Janilton Maciel <janilton@gmail.com>
2018-07-02 00:52:06 -0300

    Update url realese 1.8.0

--- Formula/dockerfile-gen.rb
+++ Formula/dockerfile-gen.rb
@@ -1,7 +1,7 @@
 class DockerfileGen < Formula
     desc "Generator Dockerfile"
     homepage "https://github.com/janiltonmaciel/dockerfile-gen"
-    url "https://github.com///releases/download/1.8.0/dockerfile-gen_1.8.0_macOS_amd64.tar.gz"
+    url "https://github.com/janiltonmaciel/dockerfile-gen/releases/download/1.8.0/dockerfile-gen_1.8.0_macOS_amd64.tar.gz"
     version "1.8.0"
     sha256 "aea9bd6165042c14d1b0d2246c7467ae393cd84bdb3b26d8ddbea40bbda870b6"
   

c49f8bd1e11c910d2cbd061367d4c5099fe23c68
goreleaserbot <goreleaser@carlosbecker.com>
2018-08-09 11:20:54 -0300

    Brew formula update for statiks version 0.3.0

--- Formula/statiks.rb
+++ Formula/statiks.rb
@@ -1,9 +1,9 @@
 class Statiks < Formula
   desc "Generator Dockerfile"
   homepage "https://github.com/janiltonmaciel/statiks"
-  url "https://github.com/janiltonmaciel/statiks/releases/download/0.2.0/statiks_0.2.0_macOS_amd64.tar.gz"
-  version "0.2.0"
-  sha256 "dac9d5bc23c03cc17474cbb0beffc2933d9f61cd01f551e727b944e62a931d6a"
+  url "https://github.com/janiltonmaciel/statiks/releases/download/0.3.0/statiks_0.3.0_macOS_amd64.tar.gz"
+  version "0.3.0"
+  sha256 "1d6df72ed4221d0f60d60ff1c1d3ad9b5b8c4f61856f3d00d606bfb61d2156bb"
 
   def install
     bin.install "statiks"
